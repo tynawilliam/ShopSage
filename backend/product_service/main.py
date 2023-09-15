7666f39577b3c8612626d4ba048d7a8aa6b37439
Darnell Eudoxie <darnelleudoxie@gmail.com>
2023-09-15 11:38:48 -0500

    Initialized Product Service

--- backend/product_service/main.py
+++ backend/product_service/main.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from flask_cors import CORS
+from flask_migrate import Migrate
+from .database import db
+from .config import Config
+from .models.product import Product
+from .models.category import Category
+from .models.product_image import ProductImage
 
 app = Flask(__name__)
+CORS(app)
+app.config.from_object(Config)
+
+db.init_app(app)
+migrate = Migrate(app, db)
 
 
 @app.route('/')
